0151a75ce7323d8df9e6d24aaee3a40fdcc4f964
ChunYu Chang <mr91217@gmail.com>
2021-08-27 13:32:29 -0400

    edit handlebars

--- config/connection.js
+++ config/connection.js
@@ -10,17 +10,17 @@ require('dotenv').config();
 //   port: 3306
 // });
 
-let sequelize;
+// let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
